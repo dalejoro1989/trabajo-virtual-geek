74da0f1ff85357e2a04fd6123960556e2309a791
dalejoroc11 <diegorojas279@hotmail.com>
2020-11-05 20:07:14 -0500

    challenge two variables with let and const

---
+++ 1-programacion-uno/challenge-Two.js
@@ -0,0 +1,6 @@
+
+/*const myAddress = 'Laureles';
+myAddress = 'La America';*/
+/* with const we can't reasignate values in the variable, we should change the asignation var to Let*/
+let myAddress = 'Laureles';
+myAddress = 'La America';
