01f94b045244748e3c7ac2f968325a0f3b903fb6
dalejoroc11 <diegorojas279@hotmail.com>
2020-11-09 15:16:52 -0500

    NaN

---
+++ 2-programacion-dos/challengeEleven.js
@@ -0,0 +1,31 @@
+/*`mango` - 2 // NaN
+
+`JavaScript loves HTML` * 100  // NaN
+
+64 * undefined // NaN*/
+
+/*
+
+2 + 3 + '5'
+
+'2' + 3 + 5
+*/
+
+/* result in console
+
+
+
+
+`mango` - 2 // NaN
+
+NaN
+`JavaScript loves HTML` * 100  // NaN
+NaN
+64 * undefined // NaN
+NaN
+2 + 3 + '5'
+"55"
+'2' + 3 + 5
+"235"
+
+*/
