7014337fb5754cca12e15f5ccda7b220437ef005
dalejoroc11 <diegorojas279@hotmail.com>
2020-11-09 10:44:49 -0500

    challenge five and six complete and fixed

--- 1-programacion-uno/challengeFive.js
+++ 1-programacion-uno/challengeFive.js
@@ -1,14 +1,7 @@
 'use strict';
-//Lola
-// Obtenemos el elemento con el que queremos trabajar usando document.querySelector()
-//const titleElement = document.querySelector('.title');
-//const listaElement = document.querySelector('.lista');
-// Cambiamos su contenido con innerHTML
-//titleElement.innerHTML = titleElement.innerHTML + listaElement.innerHTML;
-
-
-//lista Diego
-const titleElement = document.querySelector('.title');
-const lista1Element = document.querySelector('.lista1');
-// Cambiamos su contenido con innerHTML
-titleElement.innerHTML = titleElement.innerHTML + lista1Element.innerHTML;
+//passcare care the pass 
+//var local variable : visibility only in the own function
+//pass: password passcare
+let titleElement = document.querySelector('.title');
+let passElement = document.querySelector('.pass');
+passElement.innerHTML = '********';
