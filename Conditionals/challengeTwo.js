9c493d2dd70d9854a696f2d5bdc8100798849b26
dalejoroc11 <diegorojas279@hotmail.com>
2020-11-09 15:42:00 -0500

    conditionals access control with if if else else

---
+++ Conditionals/challengeTwo.js
@@ -0,0 +1,9 @@
+'use strict'; 
+var name = window.prompt("¿Cuál es tu primer nombre?"); 
+	if(name==="Diego"){
+		document.write("Bienvenido");
+	}else if(name==="karla"){
+		document.write("Bienvenida");
+	}else{
+		document.write("No eres Bienvenid@");
+	}
