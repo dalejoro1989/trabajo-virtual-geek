db91be084094574b52e96be38849f4670eea272d
dalejoroc11 <diegorojas279@hotmail.com>
2020-11-09 15:01:09 -0500

    add challenge Nine .lenght

---
+++ 2-programacion-dos/challengeNine.js
@@ -0,0 +1,5 @@
+//
+const name = 'Camila Rojas';
+console.log("el nombre de mi compañera es Camila Rojas, y esta compuesto por "+name.length+ 'caracteres');
+document.querySelector('body').innerHTML =
+`<h1>El nombre de mi compañera es Camila Rojas, y su longitud es de ${name.length} caracteres</h1>`;
