8d9b45894b7a06814a6a876f3d74a0d422f4feb6
dalejoroc11 <diegorojas279@hotmail.com>
2020-11-09 10:50:04 -0500

    ul with list js

---
+++ 1-programacion-uno/challengeSeven.js
@@ -0,0 +1,6 @@
+'use strict';
+//const local variable : visibility only in the own function
+//content content to show title image and paragraph
+const ulElement = document.querySelector('.ul');
+const content = '<li>texto1</li><li>texto2</li><li>texto3</li>';
+ulElement.innerHTML = content;
