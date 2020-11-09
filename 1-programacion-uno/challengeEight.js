4dd4c0d1c612c7463636dd510608a388361f9bfb
dalejoroc11 <diegorojas279@hotmail.com>
2020-11-09 11:07:27 -0500

    challenge Eight fixed but hidden

---
+++ 1-programacion-uno/challengeEight.js
@@ -0,0 +1,5 @@
+'use strict';
+const butt = document.querySelector('.butt');
+butt.classList.add('hidden', 'opacity');
+const buttOne = document.querySelector('.buttOne');
+butt.classList.add('hidden', 'opacity');
