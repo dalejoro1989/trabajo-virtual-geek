f2b70f61a80ea5660763126de1b727d9f624b505
dalejoroc11 <diegorojas279@hotmail.com>
2020-11-09 15:31:56 -0500

    conditionals if with alert prompt

---
+++ Conditionals/challengeOne.js
@@ -0,0 +1,14 @@
+'use strict'; 
+const const1='http://placehold.it/300x300';//charge image
+var var1='';//var empty to charge url
+const redElement = document.querySelector('.prueba');//callcss
+
+var1!=prompt('Ingrese la url: ');//alert to paid url
+
+if (var1 !='http://www.fillmurray.com/300/300'){
+    var1=const1;
+    redElement.innerHTML=` <img src="${var1}" alt="">`;//show in screem
+}
+else if(var1=='http://www.fillmurray.com/300/300'){
+    redElement.innerHTML=`<img src="${var1}" alt="">`;
+}
