80e4de6814bac3876b17105c15f7cf2e49057ece
dalejoroc11 <diegorojas279@hotmail.com>
2020-11-09 12:29:09 -0500

    add challenge five calculated alive hours simple and function

---
+++ 2-programacion-dos/challengeFive.js
@@ -0,0 +1,17 @@
+//show in console press f12
+//NO FUNCTIONS
+const age = 31;
+const hourAge = 8760;
+liveAge = age * hourAge;
+console.log('Usted ha vivido: ' + liveAge + 'horas');
+
+//inn fuction 
+function liveAges() {
+    let age = 31;
+    let hourAge = 8760;
+    
+    liveAges = (age * hourAge);
+    console.log('Usted ha Vivido: ' + liveAges + 'horas');
+}
+liveAges()
+
