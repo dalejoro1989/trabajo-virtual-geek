2d4ba3f061ed71dfcf2a723acdef3d220bf72d8e
dalejoroc11 <diegorojas279@hotmail.com>
2020-11-09 11:41:26 -0500

    challenge two css new class

--- 2-programacion-dos/challengeTwo.js
+++ 2-programacion-dos/challengeTwo.js
@@ -1,6 +1,1 @@
-const welcomeParagraph = document.querySelector('.welcomeText');
-
-// Logueo el contenido de la constante welcomeParagraph en la consola y compruebo que tiene asignado el elemento de HTML que espero antes de cambiar su contenido
-console.log('welcomeParagraph: ', welcomeParagraph);
-
-welcomeParagraph.innerHTML = 'Bienvenido Geek Boy';
+const superWave = document.querySelector('.super-wave', '.super-wawe');
