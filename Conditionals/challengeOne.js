b8b18026550845cac4726d3ea2104acace2771e2
dalejoroc11 <diegorojas279@hotmail.com>
2020-11-09 16:11:21 -0500

    conditional if with a numbers comparation

--- Conditionals/challengeOne.js
+++ Conditionals/challengeOne.js
@@ -1,6 +1,6 @@
 'use strict'; 
 const const1='http://placehold.it/300x300';//charge image
-var var1='';//var empty to charge url
+var var1='http://www.fillmurray.com/300/300';//var empty to charge url
 const redElement = document.querySelector('.prueba');//callcss
 
 var1!=prompt('Ingrese la url: ');//alert to paid url
