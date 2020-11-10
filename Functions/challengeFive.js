af32b2c6387bbacc847c54d60deb770ea75494a1
dalejoroc11 <diegorojas279@hotmail.com>
2020-11-09 19:45:08 -0500

    arrows function

---
+++ Functions/challengeFive.js
@@ -0,0 +1,5 @@
+//arrows function => Las arrow functions ("funciones flecha") de ES6 son 
+//una forma simplificada para declarar funciones anónimas. La sintaxis básica es la siguiente:
+// llaves t return implicito
+let multiplication = (numOne,numTwo) =>(numOne*numTwo);
+console.log("Resultado multiplicación es: ",multiplication(numOne=1200,numTwo=2))
