7bbcf115b3c000437063bcd1943c0c6b1e2d1dd0
dalejoroc11 <diegorojas279@hotmail.com>
2020-11-10 14:31:10 -0500

    fix events

--- Events/challengeFour.js
+++ Events/challengeFour.js
@@ -11,22 +11,25 @@ document.querySelector('button').addEventListener('click', () => {
     //integra con html
         document.querySelector('body').innerHTML +=
     //lista ordenada con peliculas enlazadas y con clase 
-    `<ol>
+    `<ul>
             <li class="inception">${inception}</li>
             <li class="theButterFlyEffect">${theButterFlyEffect}</li>
             <li class="eternalSunshineOfTheSM">${eternalSunshineOfTheSM}</li>
             <li class="blueVelvet">${blueVelvet}</li>
             <li class="split">${split}</li>
-        </ol>;`
+        </ul>;`
     //escucha y genera evento con clase al clic muestra el nimbre de cada pelicula//
 
-        document.querySelector('.inception').addEventListener('click', showName);
+     /*   document.querySelector('.inception').addEventListener('click', showName);
         document.querySelector('.theButterFlyEffect').addEventListener('click', showName);
         document.querySelector('.eternalSunshineOfTheSM').addEventListener('click', showName);
         document.querySelector('.blueVelvet').addEventListener('click', showName);
         document.querySelector('.split').addEventListener('click', showName);
+   */
+
+   document.querySelector('ul').addEventListener('click', (evento) => console.log(evento.target.innerText));
     });
 // function show in console
-    const showName = (e) => console.log(e.srcElement.innerText);
+  //  const showName = (e) => console.log(e.srcElement.innerText);
 
   
