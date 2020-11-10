5bea80fc2d5513b24096f8a67fbad616a050831d
dalejoroc11 <diegorojas279@hotmail.com>
2020-11-10 14:32:48 -0500

    Estructura de datos para un usuario

---
+++ Objects/challengeSeven.js
@@ -0,0 +1,8 @@
+const user = {};
+const job = 'Development Full sTACK'
+user.firtsName = 'DIEGO';
+user.lastName = 'ROJAS';
+user.age = 31;
+user.job = job;
+user.age = user.age+1
+user.firtsName = 'ALEJANDRO'
