ac99df07fc2b0464ba9464a8896d7a15725785f0
dalejoroc11 <diegorojas279@hotmail.com>
2020-11-10 16:05:32 -0500

    acumulator loop

---
+++ ArraysAndLoops/challengeThree.js
@@ -0,0 +1,8 @@
+//var initializate in zero
+let acc = 0;
+for (let i = 1; i < 11; i++) {
+    //var acc acumulator
+    acc = acc + 2;
+  }
+  //sow console
+  console.log('El resultado es:  ' + acc);
