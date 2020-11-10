a77016bdfd60e5f0684f4372ecf8e0eb5f9362f3
dalejoroc11 <diegorojas279@hotmail.com>
2020-11-10 09:46:47 -0500

    challenge One Events clic in button and change text in a parragraph html

---
+++ Events/challengeOne.js
@@ -0,0 +1,7 @@
+"use strict";
+//document.querySelector llama las clases .p y .button // function call addEventListener
+//js no crea funciones realiza llamadas para hacer algo con la funcion
+document.querySelector('.button').addEventListener('click',() =>{
+    //document.queryselectpr .innerText adhiere texto a screem
+document.querySelector('.p').innerText = "Mi primer click, ¡ole yo y la mujer que me parió!"
+})
