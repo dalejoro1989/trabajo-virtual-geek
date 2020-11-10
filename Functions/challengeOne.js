7bbd3b142e051fc23dca5ab4a05650b8aa3f749b
dalejoroc11 <diegorojas279@hotmail.com>
2020-11-09 19:03:03 -0500

    function multiplicaction

---
+++ Functions/challengeOne.js
@@ -0,0 +1,10 @@
+'use strict';
+//let a = window.prompt("¿write a number a: ");
+//let b = window.prompt("¿write a number b: ");
+let result;
+function multiplication(a, b) {
+result = a * b;
+   return result;
+    }
+        result = multiplication(4,-9);
+        console.log("el resultado es: " + result);
