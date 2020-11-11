1b94f47668e2b131ffd5c0a99a3ac5056dbd5a5d
dalejoroc11 <diegorojas279@hotmail.com>
2020-11-11 10:10:19 -0500

    three height

---
+++ ArraysAndLoops/challengeNine.js
@@ -0,0 +1,11 @@
+let number = 0;
+
+number = prompt('Enter tree height:');
+testOne = '';
+
+for(let i = 0; i < number; i++){
+testOne = testOne +'▲';
+console.log('' + testOne);
+
+}
+console.log('Happy Crithmas');
---
+++ ArraysAndLoops/challengeTen.js
@@ -0,0 +1,14 @@
+let number = 0;
+
+number = prompt('Enter tree height:');
+testOne = '';
+let star = '★';
+let trunk = '|'
+console.log(star);
+
+for(let i = 0; i < number; i++){
+testOne = testOne +'▲';
+console.log('' + testOne);
+}
+console.log(trunk);
+console.log('Happy Crithmas');
