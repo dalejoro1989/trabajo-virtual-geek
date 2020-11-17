92f06c6ce5d6e6f73e6ed4c7b4f692145ec01595
dalejoroc11 <diegorojas279@hotmail.com>
2020-11-17 11:15:30 -0500

    index complete

---
+++ ecommerceDevelopment/detalleProducto.js
@@ -0,0 +1,6 @@
+function  navPages() {
+    document.getElementById("productos").href="productos.html";
+    document.getElementById("detalleProductos").href="detalleProductos.html";
+    document.getElementById("pago").href="pago.html";
+    document.getElementById("carrito").href="carrito.html";
+}
---
+++ ecommerceDevelopment/index.js
@@ -0,0 +1,8 @@
+function  navPages() {
+    document.getElementById("productos").href="productos.html";
+    document.getElementById("detalleProductos").href="detalleProductos.html";
+    document.getElementById("pago").href="pago.html";
+    document.getElementById("carrito").href="carrito.html";
+    document.getElementById("mujeres").href="productos.html";
+    document.getElementById("hombres").href="productos.html";
+}
