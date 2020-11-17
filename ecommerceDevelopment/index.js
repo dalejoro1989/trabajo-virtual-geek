97ed4b084050a4c764f06cebbdb42a9243f4a6d5
dalejoroc11 <diegorojas279@hotmail.com>
2020-11-17 17:14:58 -0500

    interfaces.

--- ecommerceDevelopment/index.js
+++ ecommerceDevelopment/index.js
@@ -1,8 +1,9 @@
 function  navPages() {
     document.getElementById("productos").href="productos.html";
-    document.getElementById("detalleProductos").href="detalleProductos.html";
+    document.getElementById("detalleProductos").href="detalleProducto.html";
     document.getElementById("pago").href="pago.html";
     document.getElementById("carrito").href="carrito.html";
     document.getElementById("mujeres").href="productos.html";
     document.getElementById("hombres").href="productos.html";
+
 }
--- ecommerceDevelopment/productos.js
+++ ecommerceDevelopment/productos.js
@@ -1,6 +1,30 @@
 function  navPages() {
     document.getElementById("productos").href="productos.html";
-    document.getElementById("detalleProductos").href="detalleProductos.html";
+    document.getElementById("detalleProductos").href="detalleProducto.html";
     document.getElementById("pago").href="pago.html";
     document.getElementById("carrito").href="carrito.html";
+    document.getElementById("pUnoHombre").href="detalleProducto.html";
+    document.getElementById("pDosHombre").href="detalleProducto.html";
+    document.getElementById("pUnoMujer").href="detalleProducto.html";
+    document.getElementById("pDosMujer").href="detalleProducto.html";
+}
+
+function mostrar() {
+    div = document.getElementById('flotante');
+    div.style.display = '';
+}
+
+function cerrar() {
+    div = document.getElementById('flotante');
+    div.style.display = 'none';
+}
+
+function mostrar1() {
+    div1 = document.getElementById('flotante1');
+    div1.style.display = '';
+}
+
+function cerrar1() {
+    div1 = document.getElementById('flotante1');
+    div1.style.display = 'none';
 }
