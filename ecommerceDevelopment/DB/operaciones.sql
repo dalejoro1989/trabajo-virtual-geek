860f5502ef914646565bbfea1cc32b69930a7357
dalejoroc11 <diegorojas279@hotmail.com>
2020-11-26 09:00:48 -0500

    db

--- ecommerceDevelopment/DB/operaciones.sql
+++ ecommerceDevelopment/DB/operaciones.sql
@@ -1,11 +1,11 @@
 --insert(5)
-INSERT INTO cliente (nombre, celular, , email, sexo)
+INSERT INTO cliente (nombre, celular,  email, sexo)
 VALUES ('diego', '3104269301', 'diegorojas227@hotmail.com', 'hombre');
 --
-INSERT INTO pago (idproducto, numtarjeta, , fechavto, cvv, total)
+INSERT INTO pago (idproducto, numtarjeta,  fechavto, cvv, total)
 VALUES ('1111', '3104 4589 6321 2589', '01/21', '256', '50000');
 --
-INSERT INTO carrito (idproducto, cantidad, , precio, subtotal, total)
+INSERT INTO carrito (idproducto, cantidad,  precio, subtotal, total)
 VALUES ('3104', '2', '50000', '50000');
 --
 INSERT INTO detallepto (idproducto, tipo , precio, descripcion)
